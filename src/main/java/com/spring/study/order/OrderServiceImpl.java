6b6ceb40339ba231b3d9d64d6f6ae196a10b8256
Advanc2d <phm940916@gmail.com>
2023-07-13 20:36:29 +0900

    의존성 주입

--- src/main/java/com/spring/study/order/OrderServiceImpl.java
+++ src/main/java/com/spring/study/order/OrderServiceImpl.java
@@ -14,15 +14,49 @@ import com.spring.study.member.MemoryMemberRepository;
 @Component
 public class OrderServiceImpl implements OrderService{
 
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
+    // Autowired 생성자 1개일 시 생략가능
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
 
+/*  수정자 주입(setter 주입) 선택, 변경 가능성이 있는 의존관계에 사용
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+
+    }
+*/
+
+    /*
+    * 필드 주입
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private DiscountPolicy discountPolicy;
+    */
+
+    /*
+    * 일반 메서드 주입
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    @Autowired
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+    * */
     public MemberRepository getMemberRepository() {
         return memberRepository;
     }
